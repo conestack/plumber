8c79d9e9575d8da94d95020646a71d8d92f925b5
Robert Niederreiter <office@squarewave.at>
2017-03-14 08:45:40 +0100

    "setup.py indentation"

--- setup.py
+++ setup.py
@@ -8,37 +8,39 @@ shortdesc = "An alternative to mixin-based extension of classes."
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 
-setup(name='plumber',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: Python Software Foundation License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :; 2.7',
-          'Topic :: Software Development',
-      ],
-      keywords='',
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      url='http://github.com/bluedynamics/plumber',
-      license='Python Software Foundation License',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'setuptools',
-      ],
-      extras_require={
-          'test': [
-              'interlude',
-              'plone.testing',
-              'zope.interface',
-              'zope.deprecation',
-          ],
-      },
-      entry_points="""
-      """)
+setup(
+    name='plumber',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :; 2.7',
+        'Topic :: Software Development',
+    ],
+    keywords='',
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    url='http://github.com/bluedynamics/plumber',
+    license='Python Software Foundation License',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'interlude',
+            'plone.testing',
+            'zope.interface',
+            'zope.deprecation',
+        ],
+    },
+    entry_points="""
+    """
+)
