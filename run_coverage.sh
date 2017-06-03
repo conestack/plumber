de7ed9cf054b83dfcd308af0420f143cf6bf2c82
Robert Niederreiter <office@squarewave.at>
2017-06-03 08:23:31 +0200

    update run_coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-if [ -x "$(which python)" ]; then
-    ./py2/bin/coverage run -m plumber.tests.test_plumber
-    ./py2/bin/coverage report
-fi
-if [ -x "$(which python3)" ]; then
-    ./py3/bin/coverage run -m plumber.tests.test_plumber
-    ./py3/bin/coverage report
-fi
-if [ -x "$(which pypy)" ]; then
-    ./pypy/bin/coverage run -m plumber.tests.test_plumber
-    ./pypy/bin/coverage report
-fi
+./$1/bin/coverage run -m plumber.tests.test_plumber
+./$1/bin/coverage report
+./$1/bin/coverage html
