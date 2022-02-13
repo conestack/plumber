752775056258b207cee9fe5acc3325442805cc8f
Robert Niederreiter <office@squarewave.at>
2022-02-13 19:57:32 +0100

    Simlify Stacks. avoid copying of stacks history when parsing behaviors in plumber.__new__

--- src/plumber/plumber.py
+++ src/plumber/plumber.py
@@ -3,23 +3,16 @@ from plumber.behavior import Instructions
 
 
 class Stacks(object):
-    """Organize stacks for parsing behaviors, stored in the class' dict."""
+    """Organize stacks for parsing behaviors, stored in the class dict."""
 
     attrname = '__plumbing_stacks__'
 
     def __init__(self, dct):
-        self.dct = dct
-        self.dct.setdefault(self.attrname, dict())
-        self.stacks.setdefault('stages', dict())
-        self.stages.setdefault('stage1', dict())
-        self.stages.setdefault('stage2', dict())
-        self.stacks.setdefault('history', [])
-
-    stacks = property(lambda self: self.dct[self.attrname])
-    stages = property(lambda self: self.stacks['stages'])
-    stage1 = property(lambda self: self.stages['stage1'])
-    stage2 = property(lambda self: self.stages['stage2'])
-    history = property(lambda self: self.stacks['history'])
+        stacks = self.stacks = dct.setdefault(self.attrname, dict())
+        self.history = stacks.setdefault('history', [])
+        stages = self.stages = stacks.setdefault('stages', dict())
+        self.stage1 = stages.setdefault('stage1', dict())
+        self.stage2 = stages.setdefault('stage2', dict())
 
 
 def searchnameinbases(name, bases):
@@ -48,6 +41,7 @@ def searchnameinbases(name, bases):
 
 class Bases(object):
     """Used to search in base classes for attributes."""
+
     def __init__(self, bases):
         self.bases = bases
 
@@ -92,13 +86,12 @@ class plumber(type):
             for instruction in Instructions(behavior):
                 stage = stacks.stages[instruction.__stage__]
                 stack = stage.setdefault(instruction.__name__, [])
-                stacks.history.append(instruction)
-                if instruction not in stacks.history[:-1]:
+                # already seen instruction are ignored
+                if instruction not in stacks.history:
                     if stack:
                         instruction = stack[-1] + instruction
                     stack.append(instruction)
-                    continue
-                # already seen instruction is dropped
+                stacks.history.append(instruction)
 
         # install stage1
         for stack in stacks.stage1.values():
